7f13273abb5a342b3d9368a400a0253a9e6a9d69
Aaminahairat <121725871+Aaminahairat@users.noreply.github.com>
2024-07-09 18:15:41 +0000

    Add files via upload
    Login done, addressbook started

---
+++ dcit318assignment4-10956060/dcit318-4/dcit318-4/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace dcit318_4
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            button1 = new Button();
+            textBoxPassword = new TextBox();
+            textBoxUsername = new TextBox();
+            panel1 = new Panel();
+            label3 = new Label();
+            label2 = new Label();
+            label1 = new Label();
+            panel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button1.Location = new Point(161, 392);
+            button1.Name = "button1";
+            button1.Size = new Size(140, 50);
+            button1.TabIndex = 0;
+            button1.Text = "Login";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // textBoxPassword
+            // 
+            textBoxPassword.Location = new Point(92, 292);
+            textBoxPassword.MaximumSize = new Size(250, 31);
+            textBoxPassword.MaxLength = 8;
+            textBoxPassword.MinimumSize = new Size(150, 31);
+            textBoxPassword.Name = "textBoxPassword";
+            textBoxPassword.PasswordChar = '*';
+            textBoxPassword.PlaceholderText = "Password";
+            textBoxPassword.Size = new Size(250, 31);
+            textBoxPassword.TabIndex = 1;
+            textBoxPassword.TextChanged += textBox1_TextChanged;
+            // 
+            // textBoxUsername
+            // 
+            textBoxUsername.Location = new Point(92, 208);
+            textBoxUsername.MaxLength = 60;
+            textBoxUsername.Name = "textBoxUsername";
+            textBoxUsername.PlaceholderText = "Username";
+            textBoxUsername.Size = new Size(250, 31);
+            textBoxUsername.TabIndex = 2;
+            textBoxUsername.TextChanged += username_TextChanged;
+            // 
+            // panel1
+            // 
+            panel1.Anchor = AnchorStyles.Top;
+            panel1.BackColor = SystemColors.GradientInactiveCaption;
+            panel1.BackgroundImageLayout = ImageLayout.Stretch;
+            panel1.BorderStyle = BorderStyle.FixedSingle;
+            panel1.Controls.Add(label3);
+            panel1.Controls.Add(label2);
+            panel1.Controls.Add(label1);
+            panel1.Controls.Add(button1);
+            panel1.Controls.Add(textBoxUsername);
+            panel1.Controls.Add(textBoxPassword);
+            panel1.Location = new Point(130, 101);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(440, 553);
+            panel1.TabIndex = 3;
+            panel1.Paint += panel1_Paint;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Tahoma", 16F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.Location = new Point(161, 72);
+            label3.Name = "label3";
+            label3.Size = new Size(105, 39);
+            label3.TabIndex = 5;
+            label3.Text = "Login";
+            label3.Click += label3_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(92, 264);
+            label2.Name = "label2";
+            label2.Size = new Size(87, 25);
+            label2.TabIndex = 4;
+            label2.Text = "Password";
+            label2.Click += label2_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(92, 180);
+            label1.Name = "label1";
+            label1.Size = new Size(91, 25);
+            label1.TabIndex = 3;
+            label1.Text = "Username";
+            label1.Click += label1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.GradientInactiveCaption;
+            BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
+            ClientSize = new Size(714, 824);
+            Controls.Add(panel1);
+            ForeColor = SystemColors.Highlight;
+            MaximizeBox = false;
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button button1;
+        private TextBox textBoxPassword;
+        private TextBox textBoxUsername;
+        private Panel panel1;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+    }
+}
---
+++ dcit318assignment4-10956060/dcit318-4/dcit318-4/Form1.cs
@@ -0,0 +1,54 @@
+namespace dcit318_4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void username_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string username = textBoxUsername.Text;
+            string password = textBoxPassword.Text;
+
+            MessageBox.Show($"Usernam:{username} \nPassword: {password}");
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
